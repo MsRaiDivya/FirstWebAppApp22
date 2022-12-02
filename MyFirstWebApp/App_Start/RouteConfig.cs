25fc001c3442b379887372d73845bfc7e39aa59a
MsRaiDivya <shakshi180696@gmail.com>
2022-12-02 19:00:03 +0530

    FIRST COMMIT

---
+++ MyFirstWebApp/App_Start/RouteConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MyFirstWebApp
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            //domain/Controller_name/action_method/id
+            //techpile.in/admin/index
+            //techpile.in/admin/index/divya - same page
+            //techpile.in
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
